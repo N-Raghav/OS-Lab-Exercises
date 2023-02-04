4f6a29c5871e602b1984c40c1b806c443e09ae29
N Raghav <90966651+N-Raghav@users.noreply.github.com>
2023-02-04 10:03:42 +0530

    Add files via upload

---
+++ FisrtComeFirstServe.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+//burst time waiting time turn around time 
+void waiting_time(int process[],int bt[],int wt[],int n){
+    wt[0]=0 ;
+    for(int i=1;i<n;i++){
+        wt[i]= wt[i-1] + bt[i-1] ;
+    }
+}
+
+void turnaround_time(int process[],int bt[],int wt[],int tat[],int n){
+    for(int i=0;i<n;i++){
+        tat[i]=bt[i]+wt[i] ;
+    }
+}
+int avg_time(int arr[],int n){
+    int sum=0 ;
+    for(int i=0;i<n;i++){
+        sum+=arr[i] ;
+    }
+    int avg = sum / n ;
+    return avg ;
+}
+void main(){
+    int process[]={1,2,3} ;
+    int n=sizeof(process)/sizeof(process[0]) ;
+    int burst_time[]={10,5,1} ;
+    int waitingtime[n] ;
+    int turnaroundtime[n] ;
+    waiting_time(process,burst_time,waitingtime,n) ;
+    turnaround_time(process,burst_time,waitingtime,turnaroundtime,n) ;
+    float avg_wt,avg_tat ;
+    avg_wt = avg_time(waitingtime,n) ;
+    avg_tat=avg_time(turnaroundtime,n) ;
+    printf("Burst Time   Waiting Time   Turn Around  \n") ;
+    for(int i=0;i<n;i++){
+        printf("%d %d %d \n",burst_time[i],waitingtime[i],turnaroundtime[i]);
+    }
+    printf("The average waiting time is = %f \n",avg_wt) ;
+    printf("The average turn around time is = %f",avg_tat) ;
+}
